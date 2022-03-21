5e0e45a34295299eef20fb249118efc8bd9c9574
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 21:57:08 +0100

    1.2.1

---
+++ src/de/skullmc/cookie/tasks/ReduceClicksTask.java
@@ -0,0 +1,22 @@
+package de.skullmc.cookie.tasks;
+
+import de.skullmc.cookie.Cookie;
+import org.bukkit.Server;
+
+public class ReduceClicksTask implements Runnable{
+
+    private final Cookie plugin;
+    private final Server server;
+
+    public ReduceClicksTask(Cookie plugin, Server server) {
+        this.plugin = plugin;
+        this.server = server;
+    }
+
+
+    @Override
+    public void run() {
+        server.getOnlinePlayers().stream().filter(current -> plugin.getCookiePlayerHelper().getCookiePlayer(current) != null)
+                .forEach(current -> plugin.getCookiePlayerHelper().getCookiePlayer(current).resetClickCounter());
+    }
+}
